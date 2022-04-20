041b11839f259eedad118c2cec71dcc7e262445c
ozcanemre <ozcan.emre1608@gmail.com>
2022-04-20 15:33:36 +0200

    contenu des 2 premieres figure

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 let animeList = [{
         image:"",
-        name: "Snk",
+        name: "Attaque des titans",
         year: 2013,
-        genre: "lorem",
-        description: "lorem",
-        author:"ll",
+        genre: "Aventure, Action, Animation",
+        description: "Dans un monde ravagé par des titans mangeurs d’homme depuis plus d’un siècle, les rares survivants de l’Humanité n’ont d’autre choix pour survivre que de se barricader dans une cité-forteresse. Le jeune Eren, témoin de la mort de sa mère dévorée par un titan, n’a qu’un rêve : entrer dans le corps d’élite chargé de découvrir l’origine des Titans et les annihiler jusqu’au dernier…",
+        author:" Yasuko Kobayashi",
 
     },
     {
         image:"",
         name: "mushoku tensei",
-        year: 2013,
-        genre: "lorem",
-        description: "lorem",
-        author:"ll",
+        year: 2021,
+        genre: "Action, Aventure, Drame, Ecchi, Fantaisie, Isekai, Magie",
+        description: "Ici, je vais me transcender !' Un anonyme de 34 ans, célibataire endurci, reclus et au chômage se fait écraser par un camion peu après avoir été chassé de la maison familiale. Quelle ne fut pas sa surprise lorsqu'il se réveilla bébé dans un monde d'épées et de magie ! Rudeus, comme il s'appelait désormais, jura alors de réussir sa deuxième vie et de tirer un trait sur l'immonde déchet qu'il avait été 34 ans durant. Dès son jeune âge, il nouera de nombreux liens dans ce nouveau monde, notamment une solide amitié avec trois fillettes, avant de partir pour une aventure aussi palpitante que périlleuse. Une seconde chance lui a été accordée... et elle ne sera pas de tout repos !'",
+        author:"Rifujin na Magonote",
 
     },
     {
         image:"",
-        name: "Snk",
+        name: "Death Note",
         year: 2013,
         genre: "lorem",
         description: "lroem",
@@ -27,7 +27,7 @@ let animeList = [{
     },
     {
         image:"",
-        name: "Snk",
+        name: "Demon slayer",
         year: 2013,
         genre: "lorem",
         description: "lorem",
@@ -36,7 +36,7 @@ let animeList = [{
     },
     {
         image:"",
-        name: "Snk",
+        name: "Berserk",
         year: 2013,
         genre: "lorem",
         description: "lorem",
@@ -45,7 +45,7 @@ let animeList = [{
     },
     {
         image:"",
-        name: "Snk",
+        name: "Tower of God",
         year: 2013,
         genre: "lorem",
         description: "lorem",
@@ -54,7 +54,7 @@ let animeList = [{
     },
     {
         image: "",
-        name: "Snk",
+        name: "Kingdom",
         year: 2013,
         genre: "lorem",
         description: "lorem",
@@ -63,7 +63,7 @@ let animeList = [{
     },
     {
         image:"",
-        name: "Snk",
+        name: "Hajime no ippo",
         year: 2013,
         genre: "lorem",
         description: "lorem",
@@ -72,7 +72,7 @@ let animeList = [{
     },
     {
         image:"",
-        name: "Snk",
+        name: "Haikyuu",
         year: 2013,
         genre: "lorem",
         description: "lorem",
@@ -81,7 +81,7 @@ let animeList = [{
     },
     {
         image:"",
-        name: "hello",
+        name: "Bloody Brat",
         year: 2013,
         genre: "lorem",
         description: "lorem",
