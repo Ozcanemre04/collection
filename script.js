8882d98f509f9edca9a675de2aefd4f87719248f
ozcanemre <ozcan.emre1608@gmail.com>
2022-04-20 16:18:52 +0200

    contenu all  figures

--- script.js
+++ script.js
@@ -19,73 +19,73 @@ let animeList = [{
     {
         image:"",
         name: "Death Note",
-        year: 2013,
-        genre: "lorem",
-        description: "lroem",
-        author:"ll",
+        year: 2006 + "-" + 2007,
+        genre: "Policier, Fantastique, Thriller psychologique, Surnaturel, Mystère, Suiri",
+        description: "Light Yagami, un jeune étudiant surdoué, ramasse un jour le 'Death Note', un carnet abandonné par un dieu de la mort, Ryuk, qui apparemment s'ennuyait dans son monde. Il suffit d'écrire le nom d'une personne dans ce carnet, et celle-ci meurt. C'est ainsi qu'avec le 'Death Note' entre les mains, Light décide de débarrasser la planète de tous les criminels pour en faire un monde juste, un monde parfait.",
+        author:"Tsugumi Ōba",
 
     },
     {
         image:"",
         name: "Demon slayer",
-        year: 2013,
-        genre: "lorem",
-        description: "lorem",
-        author:"ll",
+        year: "Depuis" + 2019,
+        genre: "Fantastique, Action, Animation",
+        description: "Depuis les temps anciens, il existe des rumeurs concernant des démons mangeurs d'hommes qui se cachent dans les bois. Pour cette raison, les citadins locaux ne s'y aventurent jamais la nuit. La légende raconte aussi qu'un tueur déambule la nuit, chassant ces démons assoiffés de sang. Depuis la mort de son père, Tanjiro a pris sur lui pour subvenir aux besoins de sa famille. Malgré cette tragédie, ils réussissent à trouver un peu de bonheur au quotidien. Cependant, ce peu de bonheur se retrouve détruit le jour où Tanjiro découvre que sa famille s'est fait massacrer et que la seule survivante, sa sœur Nezuko, est devenue un démon. À sa grande surprise, Nezuko montre encore des signes d'émotion et de pensées humaines. Ainsi, commence la dure tâche de Tanjiro, celle de combattre les démons et de faire redevenir sa sœur humaine.",
+        author:"Koyoharu Gotouge",
 
     },
     {
         image:"",
         name: "Berserk",
-        year: 2013,
-        genre: "lorem",
-        description: "lorem",
-        author:"ll",
+        year: 1997,
+        genre: "Action, aventure, drame, horreur, dark fantasy, heroic fantasy",
+        description: "Dans un monde médiéval et fantastique, erre un guerrier solitaire nommé Guts, décidé à être seul maître de son destin. Autrefois contraint par un pari perdu à rejoindre les Faucons, une troupe de mercenaires dirigés par Griffith, Guts fut acteur de nombreux combats sanglants et témoin de sombres intrigues politiques. Mais il réalisa soudain que la fatalité n'existe pas et qu'il pouvait reprendre sa liberté s'il le désirait vraiment...",
+        author:"Kentarō Miura",
 
     },
     {
         image:"",
         name: "Tower of God",
-        year: 2013,
-        genre: "lorem",
-        description: "lorem",
-        author:"ll",
+        year: 2020,
+        genre: "Action, Aventure, drame, dark fantasy, mystère, science-fiction",
+        description: "Afin de retrouver Rachel, la seule personne chère à ses yeux, Bam décide de prendre tous les risques pour atteindre le sommet d'une mystérieuse tour. Pour passer chaque étage, il devra réussir un test complexe dans lequel il jouera à chaque fois sa vie. Bam en sortira t-il indemne ?",
+        author:"SIU",
 
     },
     {
         image: "",
         name: "Kingdom",
-        year: 2013,
-        genre: "lorem",
-        description: "lorem",
-        author:"ll",
+        year: 4+"juillet"+2012,
+        genre: "Action, aventure, drame, historique",
+        description: "Dans la période de l'ancienne Chine, Shin et Hyou sont des orphelins de guerre dans le royaume de Qin. Ils rêvent un jour de pouvoir se rendre sur un champ de guerre. Un jour, Hyou est amené au palais par un ministre, pris par erreur pour le roi du royaume ennemi. Il parvient cependant à revenir au village en vie pour apprendre à Shin où se trouverait le vrai roi.",
+        author:"Yasuhisa Hara",
 
     },
     {
         image:"",
         name: "Hajime no ippo",
-        year: 2013,
-        genre: "lorem",
-        description: "lorem",
-        author:"ll",
+        year: 2000,
+        genre: "Sport",
+        description: "Ippo Makunouchi est un jeune garçon de 16 ans tout ce qu'il y a de plus ordinaire. Il passe son temps libre à aider sa mère à tenir son affaire de pêche, de ce fait il n'a pas d'amis. Un jour, alors qu'il se fait maltraiter par une bande de jeunes plus âgés que lui, Takamura, un des plus grands boxeurs lui vient en aide et l'emmène dans son club de boxe pour le soigner.Une fois remis sur pieds, Ippo s'essaye à la boxe suite à une proposition de Takamura et il se révèle posséder une force incroyable. Takamura, subjugué par la force du jeune garçon lui propose de venir s'entraîner dans son club. Ippo découvre alors en lui une véritable passion pour la boxe. De plus, il est extrêmement heureux d'être aux cotés de Takamura qu'il voit comme son modèle. C'est alors le début d'une longue aventure semée d'embûches pour le jeune Ippo.",
+        author:"George Morikawa",
 
     },
     {
         image:"",
         name: "Haikyuu",
-        year: 2013,
-        genre: "lorem",
-        description: "lorem",
-        author:"ll",
+        year: 2014,
+        genre: "Comédie, sport",
+        description: "Shôyô Hinata, surnommé Shô, aime plus que tout jouer au volley-ball et ce, malgré sa petite taille. Malheureusement, suite à une sévère défaite, son club de collège a été dissous, tous les membres étant partis.Mais Shô est bien décidé à jouer de nouveau et choisit son futur lycée en fonction de son ambition. Il intègre donc le lycée Karasuno, où a joué son idole, le Petit Géant, tout en espérant faire aussi bien que lui.",
+        author:"Haruichi Furudate",
 
     },
     {
         image:"",
-        name: "Bloody Brat",
-        year: 2013,
-        genre: "lorem",
-        description: "lorem",
-        author:"ll",
+        name: "Blood lad",
+        year: 2012,
+        genre: "Action, humour noir, fantastique",
+        description: "Staz C.Vlad est un vampire pas tout à fait comme les autres : il est totalement accro à la culture humaine, et plus particulièrement à la culture japonaise. Alors le jour où il apprend qu'une japonaise s'est égarée aux enfers, il se précipite à sa rencontre. Malheureusement, la jeune Yanagi Fuyumi meurt peu après leur rencontre et se transforme en fantôme. Staz se jure alors de la ramener à la vie, quelles qu'en soit les conséquences.Le problème, c'est qu'il ne sait absolument pas comment faire.",
+        author:"Yūki Kodama",
 
     },
 ]
@@ -118,7 +118,12 @@ for (let i = 0; i < animeList.length; i++) {
     let italic = document.createElement('em');
     italic.innerHTML = animeList[i].year;
     figure.appendChild(italic)
+    let p = document.createElement('p')
+    p.innerHTML = animeList[i].author;
+    figure.appendChild(p)
 
 
 }
+
+
 
