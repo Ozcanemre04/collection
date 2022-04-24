89a66c2b8d7aacbc59d4b61bc50b34b568ea5ca3
ozcanemre <ozcan.emre1608@gmail.com>
2022-04-24 21:24:02 +0200

    ajout d1 button

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let animeList = [{
         image:"",
         name: "Attaque des titans",
         year: 2013,
-        genre: "Aventure, Action, Animation",
+        genre: ["Aventure, Action, Animation"],
         description: "Dans un monde ravagé par des titans mangeurs d’homme depuis plus d’un siècle, les rares survivants de l’Humanité n’ont d’autre choix pour survivre que de se barricader dans une cité-forteresse. Le jeune Eren, témoin de la mort de sa mère dévorée par un titan, n’a qu’un rêve : entrer dans le corps d’élite chargé de découvrir l’origine des Titans et les annihiler jusqu’au dernier…",
         author:" Yasuko Kobayashi",
 
@@ -11,7 +11,7 @@ let animeList = [{
         image:"",
         name: "mushoku tensei",
         year: 2021,
-        genre: "Action, Aventure, Drame, Ecchi, Fantaisie, Isekai, Magie",
+        genre: ["Action, Aventure, Drame, Ecchi, Fantaisie, Isekai, Magie"],
         description: "Ici, je vais me transcender !' Un anonyme de 34 ans, célibataire endurci, reclus et au chômage se fait écraser par un camion peu après avoir été chassé de la maison familiale. Quelle ne fut pas sa surprise lorsqu'il se réveilla bébé dans un monde d'épées et de magie ! Rudeus, comme il s'appelait désormais, jura alors de réussir sa deuxième vie et de tirer un trait sur l'immonde déchet qu'il avait été 34 ans durant. Dès son jeune âge, il nouera de nombreux liens dans ce nouveau monde, notamment une solide amitié avec trois fillettes, avant de partir pour une aventure aussi palpitante que périlleuse. Une seconde chance lui a été accordée... et elle ne sera pas de tout repos !'",
         author:"Rifujin na Magonote",
 
@@ -20,7 +20,7 @@ let animeList = [{
         image:"",
         name: "Death Note",
         year: 2006 + "-" + 2007,
-        genre: "Policier, Fantastique, Thriller psychologique, Surnaturel, Mystère",
+        genre: ["Policier, Fantastique, Thriller psychologique, Surnaturel, Mystère"],
         description: "Light Yagami, un jeune étudiant surdoué, ramasse un jour le 'Death Note', un carnet abandonné par un dieu de la mort, Ryuk, qui apparemment s'ennuyait dans son monde. Il suffit d'écrire le nom d'une personne dans ce carnet, et celle-ci meurt. C'est ainsi qu'avec le 'Death Note' entre les mains, Light décide de débarrasser la planète de tous les criminels pour en faire un monde juste, un monde parfait.",
         author:"Tsugumi Ōba",
 
@@ -29,7 +29,7 @@ let animeList = [{
         image:"",
         name: "Demon slayer",
         year: "Depuis " + 2019,
-        genre: "Fantastique, Action, Animation",
+        genre: ["Fantastique, Action, Animation"],
         description: "Depuis les temps anciens, il existe des rumeurs concernant des démons mangeurs d'hommes qui se cachent dans les bois. Pour cette raison, les citadins locaux ne s'y aventurent jamais la nuit. La légende raconte aussi qu'un tueur déambule la nuit, chassant ces démons assoiffés de sang. Depuis la mort de son père, Tanjiro a pris sur lui pour subvenir aux besoins de sa famille. Malgré cette tragédie, ils réussissent à trouver un peu de bonheur au quotidien. Cependant, ce peu de bonheur se retrouve détruit le jour où Tanjiro découvre que sa famille s'est fait massacrer et que la seule survivante, sa sœur Nezuko, est devenue un démon. À sa grande surprise, Nezuko montre encore des signes d'émotion et de pensées humaines. Ainsi, commence la dure tâche de Tanjiro, celle de combattre les démons et de faire redevenir sa sœur humaine.",
         author:"Koyoharu Gotouge",
 
@@ -38,7 +38,7 @@ let animeList = [{
         image:"",
         name: "Berserk",
         year: 1997,
-        genre: "Action, aventure, drame, horreur, dark fantasy, heroic fantasy",
+        genre: ["Action, aventure, drame, horreur, dark fantasy, heroic fantasy"],
         description: "Dans un monde médiéval et fantastique, erre un guerrier solitaire nommé Guts, décidé à être seul maître de son destin. Autrefois contraint par un pari perdu à rejoindre les Faucons, une troupe de mercenaires dirigés par Griffith, Guts fut acteur de nombreux combats sanglants et témoin de sombres intrigues politiques. Mais il réalisa soudain que la fatalité n'existe pas et qu'il pouvait reprendre sa liberté s'il le désirait vraiment...",
         author:"Kentarō Miura",
 
@@ -47,7 +47,7 @@ let animeList = [{
         image:"",
         name: "Tower of God",
         year: 2020,
-        genre: "Action, Aventure, drame, dark fantasy, mystère, science-fiction",
+        genre: ["Action, Aventure, drame, dark fantasy, mystère, science-fiction"],
         description: "Afin de retrouver Rachel, la seule personne chère à ses yeux, Bam décide de prendre tous les risques pour atteindre le sommet d'une mystérieuse tour. Pour passer chaque étage, il devra réussir un test complexe dans lequel il jouera à chaque fois sa vie. Bam en sortira t-il indemne ?",
         author:"SIU",
 
@@ -56,7 +56,7 @@ let animeList = [{
         image: "",
         name: "Kingdom",
         year: 4+" juillet "+2012,
-        genre: "Action, aventure, drame, historique",
+        genre: ["Action, aventure, drame, historique"],
         description: "Dans la période de l'ancienne Chine, Shin et Hyou sont des orphelins de guerre dans le royaume de Qin. Ils rêvent un jour de pouvoir se rendre sur un champ de guerre. Un jour, Hyou est amené au palais par un ministre, pris par erreur pour le roi du royaume ennemi. Il parvient cependant à revenir au village en vie pour apprendre à Shin où se trouverait le vrai roi.",
         author:"Yasuhisa Hara",
 
@@ -74,7 +74,7 @@ let animeList = [{
         image:"",
         name: "Haikyuu",
         year: 2014,
-        genre: "Comédie, sport",
+        genre: ["Comédie, sport"],
         description: "Shôyô Hinata, surnommé Shô, aime plus que tout jouer au volley-ball et ce, malgré sa petite taille. Malheureusement, suite à une sévère défaite, son club de collège a été dissous, tous les membres étant partis.Mais Shô est bien décidé à jouer de nouveau et choisit son futur lycée en fonction de son ambition. Il intègre donc le lycée Karasuno, où a joué son idole, le Petit Géant, tout en espérant faire aussi bien que lui.",
         author:"Haruichi Furudate",
 
@@ -83,7 +83,7 @@ let animeList = [{
         image:"",
         name: "Blood lad",
         year: 2012,
-        genre: "Action, humour noir, fantastique",
+        genre: ["Action, humour noir, fantastique"],
         description: "Staz C.Vlad est un vampire pas tout à fait comme les autres : il est totalement accro à la culture humaine, et plus particulièrement à la culture japonaise. Alors le jour où il apprend qu'une japonaise s'est égarée aux enfers, il se précipite à sa rencontre. Malheureusement, la jeune Yanagi Fuyumi meurt peu après leur rencontre et se transforme en fantôme. Staz se jure alors de la ramener à la vie, quelles qu'en soit les conséquences.Le problème, c'est qu'il ne sait absolument pas comment faire.",
         author:"Yūki Kodama",
 
@@ -92,7 +92,7 @@ let animeList = [{
         image:"",
         name:"Tate no Yuusha no Nariagari",
         year:2019,
-        genre:"	Action, aventure, drame, fantasy",
+        genre: ["Action, aventure, drame, fantasy"],
         description:"Naofumi se voit un jour invoqué dans un monde fantastique avec trois autres personnes afin de devenir les héros de ce monde. Dès leur arrivée, chacun se retrouve équipé d'une des quatre armes légendaires où Naofumi hérite d'un simple bouclier considéré comme le plus inutile des quatre équipements.En raison de son manque de charisme, Naofumi finit malheureusement avec une seule coéquipière tandis que les autres en avaient plusieurs. Et comme si cela ne suffisait pas, lors du deuxième jour, il a été trahi, accusé à tort et volé par ladite partenaire.Méprisé de tous, du roi aux paysans, le jeune homme ne pense désormais plus qu'à se venger !",
         author:"Amamichi Akano",
     },
@@ -100,7 +100,7 @@ let animeList = [{
         image:"",
         name:"Jujustu kaisen",
         year:2020,
-        genre:"	Adventure,Dark fantasy,Supernatural",
+        genre:["Adventure,Dark fantasy,Supernatural"],
         description:"Plus de 10 000 morts et disparus sont recensés chaque année au Japon. Les sentiments négatifs que relâchent les êtres humains sont en cause. Souffrance, regrets, humiliation : leur concentration dans un même endroit engendre des malédictions souvent mortelles... C'est ce que va découvrir Yuji Itadori, lycéen et membre du club d'occultisme. Il ne croit pas aux fantômes, mais il est doté d'une force physique hors norme qui représente un véritable atout pour les missions du groupe... jusqu'à ce que l'une d'elles prenne une mauvaise tournure. La relique qu'ils dénichent, le doigt découpé d'un démon millénaire, attire les monstres ! Sans réfléchir : le jeune homme avale la relique pour briser la malédiction ! Maintenant, il se trouve possédé par Ryômen Sukuna, le célèbre démon à deux visages. Cependant, contre toute attente, Yuji est toujours capable de garder le contrôle de son corps. Mais en dépit de cela, il est condamné à mort par l'organisation des exorcistes... Une décision qui ne pourra être repoussée qu'à une seule condition : trouver tous les doigts de Sukuna afin d'écarter la menace une bonne fois pour toutes !",
         author:"Gege Akutami",
     }
@@ -160,5 +160,24 @@ for (let i = 0; i < animeList.length; i++) {
 
 }
 
+
+let button = document.querySelector('button')
+
+button.addEventListener('click',function (){
+    button.classList.toggle('Dark')
+    body.classList.toggle('Dark')
+   if(button.classList.contains('Dark')){
+       header0.style.backgroundColor='grey';
+       button.innerHTML = 'White';
+       button.style.textShadow ="1px 1px 5px wheat"
+       header0.style.color ='white'
+   }
+   else{
+       header0.style.backgroundColor='blue'
+       button.innerHTML ='Dark'
+       header0.style.color = 'wheat'
+       button.style.textShadow = "1px 1px 5px black"
+       }
+})
 
 
