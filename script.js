4a653af4b679cf83e4f7c3038b4e0eb230aba2bf
ozcanemre <ozcan.emre1608@gmail.com>
2022-04-25 18:20:02 +0200

    fin

--- script.js
+++ script.js
@@ -1,108 +1,132 @@
 let animeList = [{
-        image:"./img/snk.jpg",
+        image: "./img/snk.jpg",
         name: "Attaque des titans",
         year: 2013,
         genre: ["Aventure, Action, Animation"],
         description: "Dans un monde ravagé par des titans mangeurs d’homme depuis plus d’un siècle, les rares survivants de l’Humanité n’ont d’autre choix pour survivre que de se barricader dans une cité-forteresse. Le jeune Eren, témoin de la mort de sa mère dévorée par un titan, n’a qu’un rêve : entrer dans le corps d’élite chargé de découvrir l’origine des Titans et les annihiler jusqu’au dernier…",
-        author:" Yasuko Kobayashi",
+        author: " Yasuko Kobayashi",
+        type:"Shōnen"
+
 
     },
     {
-        image:"./img/mushoku tensei.jpg",
+        image: "./img/mushoku tensei.jpg",
         name: "mushoku tensei",
         year: 2021,
         genre: ["Action, Aventure, Drame, Ecchi, Fantaisie, Isekai, Magie"],
         description: "Ici, je vais me transcender !' Un anonyme de 34 ans, célibataire endurci, reclus et au chômage se fait écraser par un camion peu après avoir été chassé de la maison familiale. Quelle ne fut pas sa surprise lorsqu'il se réveilla bébé dans un monde d'épées et de magie ! Rudeus, comme il s'appelait désormais, jura alors de réussir sa deuxième vie et de tirer un trait sur l'immonde déchet qu'il avait été 34 ans durant. Dès son jeune âge, il nouera de nombreux liens dans ce nouveau monde, notamment une solide amitié avec trois fillettes, avant de partir pour une aventure aussi palpitante que périlleuse. Une seconde chance lui a été accordée... et elle ne sera pas de tout repos !'",
-        author:"Rifujin na Magonote",
+        author: "Rifujin na Magonote",
+        type:"Seinen"
+
 
     },
     {
-        image:"./img/deathnote.jpg",
+        image: "./img/deathnote.jpg",
         name: "Death Note",
         year: 2006 + "-" + 2007,
         genre: ["Policier, Fantastique, Thriller psychologique, Surnaturel, Mystère"],
         description: "Light Yagami, un jeune étudiant surdoué, ramasse un jour le 'Death Note', un carnet abandonné par un dieu de la mort, Ryuk, qui apparemment s'ennuyait dans son monde. Il suffit d'écrire le nom d'une personne dans ce carnet, et celle-ci meurt. C'est ainsi qu'avec le 'Death Note' entre les mains, Light décide de débarrasser la planète de tous les criminels pour en faire un monde juste, un monde parfait.",
-        author:"Tsugumi Ōba",
+        author: "Tsugumi Ōba",
+        type:"Shōnen"
+
 
     },
     {
-        image:"./img/demonslayer.jpg",
+        image: "./img/demonslayer.jpg",
         name: "Demon slayer",
         year: "Depuis " + 2019,
         genre: ["Fantastique, Action, Animation"],
         description: "Depuis les temps anciens, il existe des rumeurs concernant des démons mangeurs d'hommes qui se cachent dans les bois. Pour cette raison, les citadins locaux ne s'y aventurent jamais la nuit. La légende raconte aussi qu'un tueur déambule la nuit, chassant ces démons assoiffés de sang. Depuis la mort de son père, Tanjiro a pris sur lui pour subvenir aux besoins de sa famille. Malgré cette tragédie, ils réussissent à trouver un peu de bonheur au quotidien. Cependant, ce peu de bonheur se retrouve détruit le jour où Tanjiro découvre que sa famille s'est fait massacrer et que la seule survivante, sa sœur Nezuko, est devenue un démon. À sa grande surprise, Nezuko montre encore des signes d'émotion et de pensées humaines. Ainsi, commence la dure tâche de Tanjiro, celle de combattre les démons et de faire redevenir sa sœur humaine.",
-        author:"Koyoharu Gotouge",
+        author: "Koyoharu Gotouge",
+        type:"Shōnen"
+
 
     },
     {
-        image:"./img/berserk.jpg",
+        image: "./img/berserk.jpg",
         name: "Berserk",
         year: 1997,
         genre: ["Action, aventure, drame, horreur, dark fantasy, heroic fantasy"],
         description: "Dans un monde médiéval et fantastique, erre un guerrier solitaire nommé Guts, décidé à être seul maître de son destin. Autrefois contraint par un pari perdu à rejoindre les Faucons, une troupe de mercenaires dirigés par Griffith, Guts fut acteur de nombreux combats sanglants et témoin de sombres intrigues politiques. Mais il réalisa soudain que la fatalité n'existe pas et qu'il pouvait reprendre sa liberté s'il le désirait vraiment...",
-        author:"Kentarō Miura",
+        author: "Kentarō Miura",
+        type:"seinen"
+
 
     },
     {
-        image:"./img/tower of god.jpg",
+        image: "./img/tower of god.jpg",
         name: "Tower of God",
         year: 2020,
         genre: ["Action, Aventure, drame, dark fantasy, mystère, science-fiction"],
         description: "Afin de retrouver Rachel, la seule personne chère à ses yeux, Bam décide de prendre tous les risques pour atteindre le sommet d'une mystérieuse tour. Pour passer chaque étage, il devra réussir un test complexe dans lequel il jouera à chaque fois sa vie. Bam en sortira t-il indemne ?",
-        author:"SIU",
+        author: "SIU",
+        type:"manhwa"
+
 
     },
     {
         image: "./img/kingdom.jpg",
         name: "Kingdom",
-        year: 4+" juillet "+2012,
+        year: 2012,
         genre: ["Action, aventure, drame, historique"],
         description: "Dans la période de l'ancienne Chine, Shin et Hyou sont des orphelins de guerre dans le royaume de Qin. Ils rêvent un jour de pouvoir se rendre sur un champ de guerre. Un jour, Hyou est amené au palais par un ministre, pris par erreur pour le roi du royaume ennemi. Il parvient cependant à revenir au village en vie pour apprendre à Shin où se trouverait le vrai roi.",
-        author:"Yasuhisa Hara",
+        author: "Yasuhisa Hara",
+        type:"seinen"
+
 
     },
     {
-        image:"./img/hajime no ippo.jpg",
+        image: "./img/hajime no ippo.jpg",
         name: "Hajime no ippo",
         year: 2000,
         genre: "Sport",
         description: "Ippo Makunouchi est un jeune garçon de 16 ans tout ce qu'il y a de plus ordinaire. Il passe son temps libre à aider sa mère à tenir son affaire de pêche, de ce fait il n'a pas d'amis. Un jour, alors qu'il se fait maltraiter par une bande de jeunes plus âgés que lui, Takamura, un des plus grands boxeurs lui vient en aide et l'emmène dans son club de boxe pour le soigner.Une fois remis sur pieds, Ippo s'essaye à la boxe suite à une proposition de Takamura et il se révèle posséder une force incroyable. Takamura, subjugué par la force du jeune garçon lui propose de venir s'entraîner dans son club. Ippo découvre alors en lui une véritable passion pour la boxe. De plus, il est extrêmement heureux d'être aux cotés de Takamura qu'il voit comme son modèle. C'est alors le début d'une longue aventure semée d'embûches pour le jeune Ippo.",
-        author:"George Morikawa",
+        author: "George Morikawa",
+        type:"Shōnen"
+
 
     },
     {
-        image:"./img/haikyuu-saison-5.jpeg",
+        image: "./img/haikyuu-saison-5.jpeg",
         name: "Haikyuu",
         year: 2014,
         genre: ["Comédie, sport"],
         description: "Shôyô Hinata, surnommé Shô, aime plus que tout jouer au volley-ball et ce, malgré sa petite taille. Malheureusement, suite à une sévère défaite, son club de collège a été dissous, tous les membres étant partis.Mais Shô est bien décidé à jouer de nouveau et choisit son futur lycée en fonction de son ambition. Il intègre donc le lycée Karasuno, où a joué son idole, le Petit Géant, tout en espérant faire aussi bien que lui.",
-        author:"Haruichi Furudate",
+        author: "Haruichi Furudate",
+        type:"Shōnen"
+
 
     },
     {
-        image:"./img/bloodybrat.jpg",
+        image: "./img/bloodybrat.jpg",
         name: "Blood lad",
         year: 2012,
         genre: ["Action, humour noir, fantastique"],
         description: "Staz C.Vlad est un vampire pas tout à fait comme les autres : il est totalement accro à la culture humaine, et plus particulièrement à la culture japonaise. Alors le jour où il apprend qu'une japonaise s'est égarée aux enfers, il se précipite à sa rencontre. Malheureusement, la jeune Yanagi Fuyumi meurt peu après leur rencontre et se transforme en fantôme. Staz se jure alors de la ramener à la vie, quelles qu'en soit les conséquences.Le problème, c'est qu'il ne sait absolument pas comment faire.",
-        author:"Yūki Kodama",
+        author: "Yūki Kodama",
+        type:"Seinen"
+
 
     },
     {
-        image:"./img/shield.jpg",
-        name:"Tate no Yuusha no Nariagari",
-        year:2019,
+        image: "./img/shield.jpg",
+        name: "Tate no Yuusha no Nariagari",
+        year: 2019,
         genre: ["Action, aventure, drame, fantasy"],
-        description:"Naofumi se voit un jour invoqué dans un monde fantastique avec trois autres personnes afin de devenir les héros de ce monde. Dès leur arrivée, chacun se retrouve équipé d'une des quatre armes légendaires où Naofumi hérite d'un simple bouclier considéré comme le plus inutile des quatre équipements.En raison de son manque de charisme, Naofumi finit malheureusement avec une seule coéquipière tandis que les autres en avaient plusieurs. Et comme si cela ne suffisait pas, lors du deuxième jour, il a été trahi, accusé à tort et volé par ladite partenaire.Méprisé de tous, du roi aux paysans, le jeune homme ne pense désormais plus qu'à se venger !",
-        author:"Amamichi Akano",
+        description: "Naofumi se voit un jour invoqué dans un monde fantastique avec trois autres personnes afin de devenir les héros de ce monde. Dès leur arrivée, chacun se retrouve équipé d'une des quatre armes légendaires où Naofumi hérite d'un simple bouclier considéré comme le plus inutile des quatre équipements.En raison de son manque de charisme, Naofumi finit malheureusement avec une seule coéquipière tandis que les autres en avaient plusieurs. Et comme si cela ne suffisait pas, lors du deuxième jour, il a été trahi, accusé à tort et volé par ladite partenaire.Méprisé de tous, du roi aux paysans, le jeune homme ne pense désormais plus qu'à se venger !",
+        author: "Amamichi Akano",
+        type:"Seinen"
+
+
     },
     {
-        image:"./img/jujustu.jpg.jpg",
-        name:"Jujustu kaisen",
-        year:2020,
-        genre:["Adventure,Dark fantasy,Supernatural"],
-        description:"Plus de 10 000 morts et disparus sont recensés chaque année au Japon. Les sentiments négatifs que relâchent les êtres humains sont en cause. Souffrance, regrets, humiliation : leur concentration dans un même endroit engendre des malédictions souvent mortelles... C'est ce que va découvrir Yuji Itadori, lycéen et membre du club d'occultisme. Il ne croit pas aux fantômes, mais il est doté d'une force physique hors norme qui représente un véritable atout pour les missions du groupe... jusqu'à ce que l'une d'elles prenne une mauvaise tournure. La relique qu'ils dénichent, le doigt découpé d'un démon millénaire, attire les monstres ! Sans réfléchir : le jeune homme avale la relique pour briser la malédiction ! Maintenant, il se trouve possédé par Ryômen Sukuna, le célèbre démon à deux visages. Cependant, contre toute attente, Yuji est toujours capable de garder le contrôle de son corps. Mais en dépit de cela, il est condamné à mort par l'organisation des exorcistes... Une décision qui ne pourra être repoussée qu'à une seule condition : trouver tous les doigts de Sukuna afin d'écarter la menace une bonne fois pour toutes !",
-        author:"Gege Akutami",
+        image: "./img/jujustu.jpg.jpg",
+        name: "Jujustu kaisen",
+        year: 2020,
+        genre: ["Adventure,Dark fantasy,Supernatural"],
+        description: "Plus de 10 000 morts et disparus sont recensés chaque année au Japon. Les sentiments négatifs que relâchent les êtres humains sont en cause. Souffrance, regrets, humiliation : leur concentration dans un même endroit engendre des malédictions souvent mortelles... C'est ce que va découvrir Yuji Itadori, lycéen et membre du club d'occultisme. Il ne croit pas aux fantômes, mais il est doté d'une force physique hors norme qui représente un véritable atout pour les missions du groupe... jusqu'à ce que l'une d'elles prenne une mauvaise tournure. La relique qu'ils dénichent, le doigt découpé d'un démon millénaire, attire les monstres ! Sans réfléchir : le jeune homme avale la relique pour briser la malédiction ! Maintenant, il se trouve possédé par Ryômen Sukuna, le célèbre démon à deux visages. Cependant, contre toute attente, Yuji est toujours capable de garder le contrôle de son corps. Mais en dépit de cela, il est condamné à mort par l'organisation des exorcistes... Une décision qui ne pourra être repoussée qu'à une seule condition : trouver tous les doigts de Sukuna afin d'écarter la menace une bonne fois pour toutes !",
+        author: "Gege Akutami",
+        type:"Shōnen"
     }
 ]
 let body = document.querySelector('body')
@@ -111,18 +135,18 @@ body.appendChild(header0)
 let h1 = document.createElement('h1')
 
 let input = document.querySelector('#searchbar')
-input.addEventListener('keyup', function firstfunction(e){
+input.addEventListener('keyup', function firstfunction(e) {
     console.log(e.target.value);
     const lowerCase = e.target.value.toLowerCase()
     const result = animeList.filter(anime => anime.name.toLocaleLowerCase().startsWith(lowerCase));
-    
+
     afficher(result)
 })
 let dom = document.querySelector('.dom')
 header0.appendChild(dom)
-header0.insertBefore(h1,header0.firstChild)
+header0.insertBefore(h1, header0.firstChild)
 dom.appendChild(input);
-h1.innerHTML="my collection"
+h1.innerHTML = "my collection"
 
 let select = document.querySelector('#select')
 dom.appendChild(select)
@@ -130,20 +154,43 @@ dom.appendChild(select)
 
 
 
-select.addEventListener('change',function(){
+select.addEventListener('change', function () {
     let vl0 = select.value
-    if(vl0==="author"){
-        
-        input.addEventListener('keyup', function(e){
+    if (vl0 === "author") {
+
+        input.addEventListener('keyup', function (e) {
             console.log(e.target.value);
             const lowerCase = e.target.value.toLowerCase()
             const result = animeList.filter(anime => anime.author.toLocaleLowerCase().startsWith(lowerCase));
-          
+
             afficher(result)
         })
+
+    } 
+    else if (vl0 === "type") {
+
+        input.addEventListener('keyup', function (e) {
+            console.log(e.target.value);
+            
+            
+            const lowerCase = e.target.value.toLowerCase()
+             
         
+            
+            const result = animeList.filter(anime => anime.type.toLocaleLowerCase().startsWith(lowerCase));
+            
+
+            afficher(result)
+            
+        })
     }
-  
+    else{
+        input.addEventListener('keyup',function(e){
+            const lowerCase =e.target.value.toLowerCase()
+            const result  =animeList.filter(anime=>anime.name.toLocaleLowerCase().startsWith(lowerCase))
+            afficher(result)
+        })
+    }
 })
 
 
@@ -151,70 +198,73 @@ select.addEventListener('change',function(){
 let main = document.createElement("main")
 body.appendChild(main)
 
-function afficher(arr){
-     
-    main.innerHTML='';
-for (let i = 0; i <arr.length; i++) {
-    let article = document.createElement('article');
-    main.appendChild(article);
-    /*img*/
-    let img = document.createElement('img')
-    img.src =arr[i].image;
-    let header  =document.createElement('header')
-    article.appendChild(header)
-    header.appendChild(img)
+function afficher(arr) {
 
-    /*name*/
-    let div = document.createElement('div')
-    header.appendChild(div)
-    let h3 = document.createElement('h3')
-    div.appendChild(h3);
-    h3.innerHTML = arr[i].name
-    
-    /*genre*/
-    let h4 = document.createElement('h4')
-    h4.innerHTML = arr[i].genre
-    div.appendChild(h4)
-    
-    /*description*/
-    let paragraphe = document.createElement('p');
-    let section = document.createElement('section')
-    paragraphe.innerHTML = arr[i].description
-    article.appendChild(section)
-    section.appendChild(paragraphe)
-    /*year*/
-    let footer = document.createElement('footer')
-    let em = document.createElement('em');
-    em.innerHTML = arr[i].year;
-    footer.appendChild(em)
-    article.appendChild(footer)
-    /*author*/ 
-    let italic = document.createElement('i')
-    italic.innerHTML = arr[i].author;
-    footer.appendChild(italic)
+    main.innerHTML = '';
+    for (let i = 0; i < arr.length; i++) {
+        let article = document.createElement('article');
+        main.appendChild(article);
+        /*img*/
+        let picture = document.createElement('picture')
+        let img = document.createElement('img')
+        img.src = arr[i].image;
+        let header = document.createElement('header')
+        article.appendChild(header)
+        header.appendChild(picture)
+        picture.appendChild(img)
+        /* figcaption*/
+        let figcaption = document.createElement('figcaption')
+        picture.appendChild(figcaption)
+        figcaption.innerHTML=arr[i].type
+
+        /*name*/
+        let div = document.createElement('div')
+        header.appendChild(div)
+        let h3 = document.createElement('h3')
+        div.appendChild(h3);
+        h3.innerHTML = arr[i].name
+
+        /*genre*/
+        let h4 = document.createElement('h4')
+        h4.innerHTML = arr[i].genre
+        div.appendChild(h4)
+
+        /*description*/
+        let paragraphe = document.createElement('p');
+        let section = document.createElement('section')
+        paragraphe.innerHTML = arr[i].description
+        article.appendChild(section)
+        section.appendChild(paragraphe)
+        /*year*/
+        let footer = document.createElement('footer')
+        let em = document.createElement('em');
+        em.innerHTML = arr[i].year;
+        footer.appendChild(em)
+        article.appendChild(footer)
+        /*author*/
+        let italic = document.createElement('i')
+        italic.innerHTML = arr[i].author;
+        footer.appendChild(italic)
 
 
-}
+    }
 }
 afficher(animeList)
 
 let button = document.querySelector('button')
 
-button.addEventListener('click',function (){
+button.addEventListener('click', function () {
     button.classList.toggle('Dark')
     body.classList.toggle('Dark')
-   if(button.classList.contains('Dark')){
-       header0.style.backgroundColor='grey';
-       button.innerHTML = 'White';
-       button.style.textShadow ="1px 1px 5px wheat"
-       header0.style.color ='white'
-   }
-   else{
-       header0.style.backgroundColor='midnightblue'
-       button.innerHTML ='Dark'
-       header0.style.color = 'wheat'
-       button.style.textShadow = "1px 1px 5px black"
-       }
+    if (button.classList.contains('Dark')) {
+        header0.style.backgroundColor = 'grey';
+        button.innerHTML = 'White';
+        button.style.textShadow = "1px 1px 5px wheat"
+        header0.style.color = 'white'
+    } else {
+        header0.style.backgroundColor = 'midnightblue'
+        button.innerHTML = 'Dark'
+        header0.style.color = 'wheat'
+        button.style.textShadow = "1px 1px 5px black"
+    }
 })
-
-
